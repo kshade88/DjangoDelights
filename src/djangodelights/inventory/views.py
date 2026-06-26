25f4860de22ea37af527d8e3b020fc235548973d
Kyle's MacBook Air <macbookair@Kyless-MacBook-Air.local>
2026-06-25 20:45:27 -0400

    Added sign up and set urls files

--- src/djangodelights/inventory/views.py
+++ src/djangodelights/inventory/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, logout, authenticate
 from django.urls import reverse_lazy
 
 # Create your views here.
@@ -10,4 +10,12 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     template_name = 'registration/signup.html'
     success_url = reverse_lazy('login')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+ 
+
+
 
